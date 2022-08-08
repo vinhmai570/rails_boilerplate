30adb14b9e4d7c4551c65cad13d0131de3088cc3
vinhmai570 <vinhmai570@gmail.com>
2022-08-08 07:37:51 +0700

    update config puma

--- config/puma.rb
+++ config/puma.rb
@@ -19,7 +19,7 @@ port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment 'production'
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
